92b63bdc03a12e580ab5167a41c97f447a04fa90
oguntayomicheal <oguntayomicheal@gmail.com>
2023-03-21 23:58:42 +0100

    Add games table to schema file

--- database/schema.sql
+++ database/schema.sql
@@ -4,3 +4,19 @@ CREATE TABLE author(
   last_name VARCHAR(100),
   items ARRAY
 );
+
+CREATE TABLE games (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  last_played_at DATE,
+  publish_date Date,
+  archived BOOLEAN,
+  multiplayer BOOLEAN,
+  label_id INT,
+  genre_id INT,
+  source_id INT,
+  author_id INT,
+  FOREIGN KEY (author_id) REFERENCES authors(id),
+  FOREIGN KEY (label_id) REFERENCES labels(id),
+  FOREIGN KEY (source_id) REFERENCES sources(id),
+  FOREIGN KEY (genre_id) REFERENCES genres(id),
+);
