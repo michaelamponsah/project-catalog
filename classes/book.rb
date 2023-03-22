528515b0487900b030fd0c93762c46ae01574af8
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-03-22 08:41:06 +0000

    Add a can_be_archived?() method to Book class

--- classes/book.rb
+++ classes/book.rb
@@ -1,10 +1,16 @@
 require_relative './item'
 
 class Book < Item
-  def initialize(publish_date, publiser, cover_state, archived)
+  def initialize(publish_date, publiser, cover_state, archived: false)
     super(publish_date, archived)
 
     @publisher = publiser
     @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    return true if super.can_be_archived? || cover_state == 'bad'
+
+    false
   end
 end
